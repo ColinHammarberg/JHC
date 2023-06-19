1d4de7eac968c001d0da71e4b2f977538b80d904
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-06-19 16:18:43 +0200

    deploy

--- jhc-frontend-app/src/components/Bookings.js
+++ jhc-frontend-app/src/components/Bookings.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './styles/Bookings.scss';
 import CalendyWidget from './CalendyWidget';
+import { Avatar } from './Avatar';
 
 export const Bookings = () => {
 
     return (
         <div className="booking">
-            <CalendyWidget />
+            <div className="avatar">
+                <Avatar />
+            </div>
+            <div className="calendy">
+                <CalendyWidget />
+            </div>
         </div>
     )
 }
