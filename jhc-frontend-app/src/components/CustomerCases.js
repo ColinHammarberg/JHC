ec0fd4ca098a35fd9f08aeb6affc13b4767097ac
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-07-11 17:33:03 +0200

    update videos

--- jhc-frontend-app/src/components/CustomerCases.js
+++ jhc-frontend-app/src/components/CustomerCases.js
@@ -1,56 +1,70 @@
-import React from 'react';
-// import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
+import React, { useState } from 'react';
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import './styles/CustomerCases.scss';
 import Header from './Header';
-import Image from '../images/Jonas-5.png';
+import Image from '../images/jonas.jpeg';
 
 const videoData = [
   {
-    src: 'https://www.youtube.com/embed/video1',
+    src: 'https://www.youtube.com/embed/video1?autoplay=1',
     thumbnail: Image,
     alt: 'Thumbnail 1',
-    className: "video-1"
+    className: 'video-1'
   },
   {
-    src: 'https://www.youtube.com/embed/video2',
+    src: 'https://www.youtube.com/embed/video2?autoplay=1',
     thumbnail: Image,
     alt: 'Thumbnail 2',
-    className: "video-2"
+    className: 'video-2'
   },
   {
-    src: 'https://www.youtube.com/embed/video3',
+    src: 'https://www.youtube.com/embed/video3?autoplay=1',
     thumbnail: Image,
     alt: 'Thumbnail 3',
-    className: "video-3"
+    className: 'video-3'
   },
   {
-    src: 'https://www.youtube.com/embed/video4',
+    src: 'https://www.youtube.com/embed/video4?autoplay=1',
     thumbnail: Image,
     alt: 'Thumbnail 4',
-    className: "video-4"
-  },
+    className: 'video-4'
+  }
 ];
 
 function CustomerCases() {
+  const [playingVideo, setPlayingVideo] = useState(null);
+
+  const handlePressPlay = (index) => {
+    setPlayingVideo(index);
+  };
+
   return (
     <div className="video-container">
       <Header />
       <div className="videos">
         {videoData.map((video, index) => (
-            <div className={`video-wrapper ${video.className}`} key={index}>
-                <iframe
-                    src={video.src}
-                    title={video.alt}
-                    allow="autoplay; encrypted-media"
-                    allowFullScreen
-                ></iframe>
-                {/* <img
-                    src={video.thumbnail}
-                    alt={video.alt}
-                    className="video-thumbnail"
+          <div className={`video-wrapper ${video.className}`} key={index}>
+            {playingVideo === index ? (
+              <iframe
+                src={video.src}
+                title={video.alt}
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              ></iframe>
+            ) : (
+              <div className="image-thumbnail">
+                <img
+                  src={video.thumbnail}
+                  alt={video.alt}
+                  className="video-thumbnail"
                 />
-                <PlayCircleFilledWhiteIcon /> */}
-            </div>
+                <PlayCircleFilledWhiteIcon
+                  className="play-button"
+                  onClick={() => handlePressPlay(index)}
+                />
+              </div>
+            )}
+          </div>
         ))}
       </div>
     </div>
