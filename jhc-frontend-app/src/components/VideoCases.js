4ea1f86ab89a40d828d8c0ffe8926de0f5aff977
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-07-26 20:17:29 +0200

    update videocases

--- jhc-frontend-app/src/components/VideoCases.js
+++ jhc-frontend-app/src/components/VideoCases.js
@@ -2,32 +2,26 @@ import React, { useState } from 'react';
 import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 import './styles/VideoCases.scss';
 import Header from './Header';
-import Urs from '../images/Urs-thumbnail.png';
-import Image from '../images/jonas.jpeg';
 import Tabs, { useTabs } from './Tabs';
 
 const inspirationVideos = [
   {
     src: 'https://www.youtube.com/embed/D2QVjE7OPio',
-    thumbnail: Image,
     alt: 'Thumbnail 1',
     className: 'video-1'
   },
   {
     src: 'https://www.youtube.com/embed/DZxaV_15EPM',
-    thumbnail: Image,
     alt: 'Thumbnail 2',
     className: 'video-2'
   },
   {
     src: 'https://www.youtube.com/embed/A6CPhJ8GWhU',
-    thumbnail: Image,
     alt: 'Thumbnail 3',
     className: 'video-3'
   },
   {
     src: 'https://www.youtube.com/embed/ZYh86E_CbiY',
-    thumbnail: Image,
     alt: 'Thumbnail 4',
     className: 'video-4'
   }
@@ -36,25 +30,21 @@ const inspirationVideos = [
 const PublicVideos = [
   {
     src: 'https://www.youtube.com/embed/3Ebi8fr88y4',
-    thumbnail: Urs,
     alt: 'Thumbnail 1',
     className: 'video-1'
   },
   {
     src: 'https://www.youtube.com/embed/F9ZAW43M9HQ',
-    thumbnail: Urs,
     alt: 'Thumbnail 2',
     className: 'video-2'
   },
   {
     src: 'https://www.youtube.com/embed/TPrge9pPPpA',
-    thumbnail: Urs,
     alt: 'Thumbnail 3',
     className: 'video-3'
   },
   {
     src: 'https://www.youtube.com/embed/ZlbiEgNAiEc',
-    thumbnail: Urs,
     alt: 'Thumbnail 4',
     className: 'video-4'
   }
@@ -99,9 +89,7 @@ function VideoCases() {
               ></iframe>
             ) : (
               <div className="image-thumbnail">
-                <img
-                  src={video.thumbnail}
-                  alt={video.alt}
+                <div
                   className="video-thumbnail"
                 />
                 <PlayCircleFilledWhiteIcon
