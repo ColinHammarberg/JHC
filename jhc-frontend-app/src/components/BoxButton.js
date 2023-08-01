d99c6abe9213ca10d0d11722e8c5ce01aaeb7bcb
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-08-01 13:00:22 +0200

    update boxbutton

--- jhc-frontend-app/src/components/BoxButton.js
+++ jhc-frontend-app/src/components/BoxButton.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import './styles/BoxButton.scss';
 
 
 function BoxButton({ variant, onClick, className }) {
+    const [isClicked, setIsClicked] = useState(false);
 
     function handleOnClick() {
         onClick();
+        setIsClicked(true);
+    }
+
+    function onAnimationEnd() {
+        setIsClicked(false);
     }
 
     return (
-        <Button className={`box-button ${className}`} onClick={handleOnClick}>
+        <Button className={`box-button ${className} ${isClicked ? 'pop-animation' : ''}`} onClick={handleOnClick} onAnimationEnd={onAnimationEnd}>
             {typeof variant === 'object' ? variant : <img src={variant} alt='' />}
         </Button>
     )
